7ab042c058aaae59b2eeef2e2f5b767c8368b586
Chris Thompson <chris@idea7.cc>
2026-03-24 21:33:02 -0400

    Getting rid of old files from prior version.

--- fellowship42-app/src/lib/getPayloadClient.ts
+++
@@ -1,9 +0,0 @@
-import { getPayload } from 'payload'
-
-import config from '@/payload.config'
-
-export const getPayloadClient = async () => {
-  const payloadConfig = await config
-  return getPayload({ config: payloadConfig })
-}
-
